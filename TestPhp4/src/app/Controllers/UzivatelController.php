01f3f4e83c56bdf8e6200f5118b10d3e53f606be
Jan_Specian <jan.specian@tul.cz>
2018-04-25 22:45:13 +0200

    model Uzivatel

--- TestPhp4/src/app/Controllers/UzivatelController.php
+++ TestPhp4/src/app/Controllers/UzivatelController.php
@@ -6,8 +6,38 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 use \Interop\Container\ContainerInterface as ContainerInterface;
 use PDO;
+use App\Models\Uzivatel;
+use App\Models\RoleUzivatele;
 
-class UzivatelController extends ApiController{
+class UzivatelController extends ApiController {
+
+    public function uzivatele(Request $request, Response $response, array $args) {
+
+        $sql = "
+        SELECT * 
+        FROM Uzivatel
+        INNER JOIN RoleUzivatele ON RoleUzivatele.idRoleUzivatele = Uzivatel.idRoleUzivatele";
+        $stmt = $this->container->db->prepare($sql);
+        $stmt->execute();
+        $results = $stmt->fetchAll();
+        
+        $uzivatele = [];
+        foreach ($results as $obj){
+            $role = new RoleUzivatele($obj['nazevRole'], $obj['idRoleUzivatele']);
+            $uzivatele[] = new Uzivatel($obj['id'],$obj['nickName'],$obj['login'],$obj['celeJmeno'],$role);
+        }
+        $response->getBody()->write("hovno");
+        return $response;
+    }
+
+    /**
+     * Assebly model from DB
+     */
+    private function assemblyDTO($object_input) {
+
+        $u = new Uzivatel($object_input['id']);
+        return $u;
+    }
 
     public function getUserDetailById(Request $request, Response $response, array $args) {
         $id = $args["id"];  //important "" not ''
@@ -19,9 +49,10 @@ class UzivatelController extends ApiController{
         $result = $stmt->fetch();
 
         $response->getBody()->write(json_encode($result, JSON_UNESCAPED_UNICODE));
+        //$response->getBody()->write($result);
         return $response;
     }
-    
+
     protected function getUserById($id) {
         $sql = "SELECT * FROM Uzivatel WHERE Uzivatel.idUzivatel=?";
 
@@ -33,58 +64,46 @@ class UzivatelController extends ApiController{
         return $response;
     }
 
-    public function uzivatele(Request $request, Response $response, array $args) {
-
-        $sql = "SELECT * FROM Uzivatel";
-        $stmt = $this->container->db->prepare($sql);
-        $stmt->execute();
-        $result = $stmt->fetchAll();        
-        $response->getBody()->write(json_encode($result, JSON_UNESCAPED_UNICODE));
-        return $response;
-    }
-    
     public function saveNewUser(Request $request, Response $response, array $args) {
-               
+
         $body = $request->getParsedBody();
         $nickName = $body['nickName'];
         $celeJmeno = $body['celeJmeno'];
         $idRoleUzivatele = $body['idRoleUzivatele'];
-                
+
         //vlozeni do databaze
         $sql = 'INSERT INTO Uzivatel VALUES '
                 . '(NULL, ?, ?, \'Adresa\', NOW(), 999999, ?, NULL, NULL, NULL, NULL)';
 
         $stmt = $this->container->db->prepare($sql);
-        
-        try{
-            $stmt->bindParam(1,$body['nickName'], PDO::PARAM_STR);
-            $stmt->bindParam(2,$body['celeJmeno'], PDO::PARAM_STR);
-            $stmt->bindParam(3,$body['idRoleUzivatele'], PDO::PARAM_INT);
+
+        try {
+            $stmt->bindParam(1, $body['nickName'], PDO::PARAM_STR);
+            $stmt->bindParam(2, $body['celeJmeno'], PDO::PARAM_STR);
+            $stmt->bindParam(3, $body['idRoleUzivatele'], PDO::PARAM_INT);
 
             $stmt->execute();
-        
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             return $response->withStatus(400);
         }
-        
+
         //navratova hodnota
         return $response->getBody()->write("Inserted");
-    }   
-    
+    }
+
     public function updateUser(Request $request, Response $response, array $args) {
-               
+
         $id = $args['id'];
         //vlozeni do databaze
-        
+
         foreach ($request->getParsedBody() as $key => $val) {
             try {
-                $sql = 'UPDATE Uzivatel SET '.$key.'=? WHERE idUzivatel = ?';
+                $sql = 'UPDATE Uzivatel SET ' . $key . '=? WHERE idUzivatel = ?';
                 $stmt = $this->container->db->prepare($sql);
                 //$stmt->bindParam(1, $key, PDO::PARAM_STR);
                 $stmt->bindParam(1, $val, PDO::PARAM_STR);
                 $stmt->bindParam(2, $id, PDO::PARAM_INT);
                 $stmt->execute();
-                
             } catch (PDOException $e) {
                 return $response->withStatus(400);
             }
@@ -92,27 +111,26 @@ class UzivatelController extends ApiController{
 
         //navratova hodnota
         return $response->getBody()->write("Updated");
-    }   
-    
+    }
+
     public function deleteUser(Request $request, Response $response, array $args) {
-        
+
         echo "deleting user" . $request->getAttribute("id");
-        
+
         $id = $args['id'];
         //vlozeni do databaze
         $sql = 'DELETE FROM Uzivatel WHERE idUzivatel = ?';
         $stmt = $this->container->db->prepare($sql);
-        
-        try{
-            
-            $stmt->bindParam(1,$id, PDO::PARAM_INT);
+
+        try {
+
+            $stmt->bindParam(1, $id, PDO::PARAM_INT);
             $stmt->execute();
-            
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             return $response->withStatus(400);
         }
         //navratova hodnota
         return $response->getBody()->write("Deleted");
-    }   
-    
+    }
+
 }
--- TestPhp4/src/app/Models/Uzivatel.php
+++ TestPhp4/src/app/Models/Uzivatel.php
@@ -8,6 +8,8 @@
 
 namespace App\Models;
 
+use App\Models\RoleUzivatele;
+
 class Uzivatel{
     
     private $id;
@@ -15,5 +17,35 @@ class Uzivatel{
     private $login;
     private $celeJmeno;
     private $role;
+    
+    function __construct($id, $nickName, $login, $celeJmeno, RoleUzivatele $role) {
+        $this->id = $id;
+        $this->nickName = $nickName;
+        $this->login = $login;
+        $this->celeJmeno = $celeJmeno;
+        $this->role = $role;
+    }
+    
+    function getId() {
+        return $this->id;
+    }
+
+    function getNickName() {
+        return $this->nickName;
+    }
+
+    function getLogin() {
+        return $this->login;
+    }
+
+    function getCeleJmeno() {
+        return $this->celeJmeno;
+    }
+
+    function getRole() {
+        return $this->role;
+    }
+
+    
     
 }
